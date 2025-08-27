b789b7f99199ce3427ac9916787b09845583ffea
LucasGGamerM <LucasGGamerM@protonmail.com>
2025-08-27 17:40:08 -0300

    fix(neovim): use proper syntax on keymap definition

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -8,7 +8,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts, { desc = 'See diagnostic properly' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'See diagnostic properly' })
 
 -- Buffer switch with tabs
 vim.keymap.set('n', '<Tab>', vim.cmd.bn, opts, { desc = 'Go to next buffer' })
