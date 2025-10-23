e8d53bd645a6429a7d2f67c3dfa69bf088cf2a74
LucasGGamerM <LucasGGamerM@protonmail.com>
2025-10-23 20:54:27 -0300

    chore(neovim): use mini.surround defaults

--- nvim/.config/nvim/lua/kickstart/plugins/mini.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/mini.lua
@@ -11,24 +11,7 @@ return {
       require('mini.ai').setup { n_lines = 500 }
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      require('mini.surround').setup {
-        custom_surroundings = {
-          ['('] = { output = { left = '( ', right = ' )' } },
-          ['['] = { output = { left = '[ ', right = ' ]' } },
-          ['{'] = { output = { left = '{ ', right = ' }' } },
-          ['<'] = { output = { left = '< ', right = ' >' } },
-        },
-        mappings = {
-          add = 'ys',
-          delete = 'ds',
-          find = '',
-          find_left = '',
-          highlight = '',
-          replace = 'cs',
-          update_n_lines = '',
-        },
-        search_method = 'cover_or_next',
-      }
+      require('mini.surround').setup {}
 
       -- Remap adding surrounding to Visual mode selection
       vim.api.nvim_set_keymap('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { noremap = true })
