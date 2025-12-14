f2758e3b559cd66e375f44c2cff61af535037812
LucasGGamerM <LucasGGamerM@protonmail.com>
2025-12-14 15:01:56 -0300

    feat(neovim): include java-test mason package
    May junit bless us all

--- nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -269,6 +269,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'java-test',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
